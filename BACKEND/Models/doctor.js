513fa98a9c197ec0a03303ca1a717a801b3a7557
Imalsha Colambage <imalshacolambage171@gmail.com>
2024-04-12 17:30:50 +0530

    consultantcare/models/doctor.js

---
+++ BACKEND/Models/doctor.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const doctorSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+    },
+
+    name: {
+        type: String,
+        required: true,
+    },
+    specialty: {
+        type: String,
+        required: true,
+    },
+});
+const doctor = mongoose.model("doctor", doctorSchema);
+
+module.exports = doctor;
